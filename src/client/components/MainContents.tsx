2eba7e21287be36d48a79b85f9d42cd982cfd202
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-06-10 23:28:28 +0900

    ランキングできたわ

--- src/client/components/MainContents.tsx
+++ src/client/components/MainContents.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { media } from '../style/media';
 import { DataTable } from './DataTable';
+import { Ranking } from './Ranking';
 
 const MainContents: React.FC = () => {
   return (
     <MainWrapper>
       <DataTable />
+      <Ranking />
     </MainWrapper>
   );
 };
@@ -18,6 +20,6 @@ const MainWrapper = styled.main`
   padding: 20px 20px 0;
   ${media.lessThan('medium')`
     flex-direction: column;
-    padding: 20px 10px 10px;
+    padding: 20px 0;
   `}
 `;
---
+++ src/client/components/Ranking.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+import { useSelector } from '../hooks/useSelector';
+import { media } from '../style/media';
+
+const Ranking: React.FC = () => {
+  const { fontSize } = useSelector(({ setting }) => setting);
+
+  return (
+    <Wrapper>
+      <Head>
+        <HeadText fontSize={fontSize}>ランキング</HeadText>
+      </Head>
+      <Players fontSize={fontSize}>
+        <Row>
+          <Order fontSize={fontSize}>1</Order>
+          <Name fontSize={fontSize}>ざえもん</Name>
+          <Cell fontSize={fontSize}>+210</Cell>
+        </Row>
+        <Row>
+          <Order fontSize={fontSize}>2</Order>
+          <Name fontSize={fontSize}>あああ</Name>
+          <Cell fontSize={fontSize}>+100</Cell>
+        </Row>
+        <Row>
+          <Order fontSize={fontSize}>3</Order>
+          <Name fontSize={fontSize}>しんにじえも</Name>
+          <Cell fontSize={fontSize}>+20</Cell>
+        </Row>
+        <Row>
+          <Order fontSize={fontSize}>4</Order>
+          <Name fontSize={fontSize}>ぶりぶり</Name>
+          <Cell fontSize={fontSize}>-40</Cell>
+        </Row>
+      </Players>
+    </Wrapper>
+  );
+};
+
+export { Ranking };
+
+const Wrapper = styled.div`
+  margin-left: 20px;
+  width: 400px;
+  min-width: 400px;
+  ${media.lessThan('medium')`
+    width: 100%;
+    min-width: 100%;
+    margin-left: 0;
+    margin-top: 20px;
+  `}
+`;
+
+const Head = styled.div`
+  padding: 16px;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+`;
+
+const HeadText = styled(Typography)<{ fontSize: number }>`
+  font-size: ${({ fontSize }) => fontSize}px;
+  color: ${({ theme }) => theme.palette.primary.contrastText};
+  font-weight: 600;
+  margin: 0;
+  padding: 0;
+`;
+
+const Players = styled.div<{ fontSize: number }>`
+  max-height: calc(100vh - ${({ fontSize }) => fontSize * 1.1}px - 148px);
+  ${media.greaterThan('medium')`
+    overflow-y: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `}
+  ${media.lessThan('medium')`
+    max-height: auto;
+  `}
+`;
+
+const Row = styled.div`
+  display: flex;
+  &:nth-of-type(even) {
+    background-color: ${({ theme }) => theme.palette.primary.light};
+  }
+`;
+
+const Cell = styled(Typography)<{ fontSize: number }>`
+  font-size: ${({ fontSize }) => fontSize}px;
+  text-align: right;
+  padding: 12px 8px;
+  line-height: ${({ fontSize }) => fontSize + 4}px;
+`;
+
+const Order = styled(Cell)`
+  min-width: ${({ fontSize }) => fontSize}px;
+  max-width: ${({ fontSize }) => fontSize}px;
+`;
+
+const Name = styled(Cell)`
+  flex-grow: 1;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  text-align: left;
+`;
