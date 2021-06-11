1a0ab4491ae24064c067f1f3f29af5a0ab2a5d8b
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-06-11 13:56:12 +0900

    TypeORM導入

---
+++ ormconfig.js
@@ -0,0 +1,26 @@
+const { config } = require('dotenv');
+
+config();
+
+module.exports = [
+  {
+    type: 'postgres',
+    name: 'default',
+    host: '127.0.0.1',
+    port: 5432,
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: 'skullking',
+    synchronize: false,
+    logging: false,
+    connectTimeout: 30 * 1000,
+    acquireTimeout: 30 * 1000,
+    entities: [__dirname + '/dist/server/entity/**/*.js'],
+    migrations: [__dirname + '/dist/server/migration/**/*.js'],
+    cli: {
+      entitiesDir: 'src/server/entity',
+      migrationsDir: 'src/server/migration',
+      subscribersDir: 'src/server/subscriber',
+    }
+  },
+];
